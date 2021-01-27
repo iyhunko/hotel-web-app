aad3ca146da26ccb9c3ad66a4cea54dee4839500
iyhunko <iyhunko@gmail.com>
2021-01-27 23:50:30 +0200

    Add User model/repository/service

---
+++ src/main/java/com/iyhunko/hotel/models/User.java
@@ -0,0 +1,74 @@
+package com.iyhunko.hotel.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 30)
+    private String firstname;
+
+    @Column(nullable = false, length = 30)
+    private String lastname;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(length = 150)
+    private String roles;
+
+    public void setEmail(String username) {
+        this.email = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}

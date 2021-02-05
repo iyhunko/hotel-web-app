228f75f9f6e499a588d38b7ee913bbdb79705c07
iyhunko <iyhunko@gmail.com>
2021-02-06 01:54:51 +0200

    Add bookings seeder

---
+++ src/main/resources/db/migration/V2.4__seed_bookings.sql
@@ -0,0 +1,62 @@
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(22, 2, 1, null, '2021-02-01', '2021-02-03', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(2, 3, 2, null, '2021-03-01', '2021-03-05', 'payed');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(3, 4, 3, null, '2021-04-02', '2021-04-03', 'payment');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(4, 5, 4, null, '2021-03-01', '2021-03-04', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(5, 6, 5, null, '2021-03-02', '2021-03-05', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(6, 7, 1, null, '2021-03-09', '2021-03-06', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(7, 8, 6, null, '2021-03-08', '2021-03-13', 'payment');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(8, 9, 7, null, '2021-03-07', '2021-03-23', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(9, 10, 8, null, '2021-03-05', '2021-03-14', 'payed');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(10, 11, 9, null, '2021-03-4', '2021-03-13', 'payment');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(11, 22, 10, null, '2021-03-03', '2021-03-23', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(12, 13, 11, null, '2021-03-01', '2021-03-03', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(13, 14, 12, null, '2021-03-04', '2021-03-13', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(14, 15, 13, null, '2021-03-08', '2021-03-23', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(15, 16, 14, null, '2021-03-02', '2021-03-05', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(16, 17, 15, null, '2021-03-03', '2021-03-06', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(17, 18, 16, null, '2021-03-06', '2021-03-07', 'payment');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(18, 19, 17, null, '2021-03-04', '2021-03-21', 'payed');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(19, 20, 18, null, '2021-03-05', '2021-03-22', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(20, 21, 19, null, '2021-03-03', '2021-03-23', 'created');
+
+insert into bookings (id, user_id, room_id, payment_id, checkin_date, checkout_date, status)
+values(21, 22, 20, null, '2021-03-02', '2021-03-12', 'payed');
